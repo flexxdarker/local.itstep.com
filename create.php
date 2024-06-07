4ff6421de2abdfe7268ca3c7672113de6a622937
flexxdarker <ksenzhakmax@gmail.com>
2024-06-07 16:20:49 +0300

    adding user done

--- create.php
+++ create.php
@@ -1,3 +1,26 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST") {
+    include_once $_SERVER["DOCUMENT_ROOT"]."/connection_database.php";
+    $folderName = $_SERVER['DOCUMENT_ROOT'].'/'. MEDIA;// Задайте ім'я папки, яку ви хочете створити
+    if (!file_exists($folderName)) {
+        mkdir($folderName, 0777); // Створити папку з правами доступу 0777
+    }
+    $ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $fileName = uniqid() . '.' .$ext;
+    $uploadfile = $folderName ."/". $fileName;
+    move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile);
+    $name =  $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $sql = 'INSERT INTO tbl_users (name, email, phone, image) VALUES (:name, :email, :phone, :image)';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['name' => $name, 'email' => $email, 'phone' => $phone, 'image' => $fileName]);
+
+    header('Location: /');
+    exit();
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,70 +29,51 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
 </head>
 <body>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "localhost"; // Database server name or IP
-    $username = "root"; // Database username
-    $password = ""; // Database password
-    $dbname = "local.itstep.com"; // Database name
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Get form data
-    $name = $conn->real_escape_string($_POST['Username']);
-    $email = $conn->real_escape_string($_POST['Email']);
-    $imageUrl = $conn->real_escape_string($_POST['ImageUrl']);
-    $phoneNumber = $conn->real_escape_string($_POST['PhoneNumber']);
-
-    // SQL query to insert data
-    $sql = "INSERT INTO `tbl_users` (name, email, phone, imageUrl,) VALUES ('$name', '$email', '$phoneNumber', '$imageUrl')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-}
-
-?>
-<form action="create.php" method="POST">
+<main>
     <div class="container">
-        <div class="mb-3">
-            <div class="input-group">
-                <input type="text" class="form-control" id="Username" name="Username" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <h1 class="text-center">Add User</h1>
+        <form class="col-md-6 offset-md-3 was-validated" method="POST" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name:</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+                <div class="valid-feedback">Valid</div>
+                <div class="invalid-feedback">Please fill your name</div>
             </div>
-            <div class="form-text">Enter your full name</div>
-        </div>
-        <div class="mb-3">
-            <div class="input-group">
-                <input type="text" class="form-control" id="ImageUrl" name="ImageUrl" placeholder="ImageUrl" aria-label="ImageUrl" aria-describedby="basic-addon1">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+                <div class="valid-feedback">Valid</div>
+                <div class="invalid-feedback">Please fill your email</div>
             </div>
-            <div class="form-text">Enter image Url</div>
-        </div>
-        <div class="mb-3">
-            <div class="input-group">
-                <input type="text" class="form-control" id="Email" name="Email" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone:</label>
+                <input type="text" class="form-control" id="phone" name="phone" required>
+                <div class="valid-feedback">Valid</div>
+                <div class="invalid-feedback">Please fill your phone</div>
             </div>
-            <div class="form-text">Enter your email</div>
-        </div>
-        <div class=" mb-3">
-            <div class="input-group">
-                <input type="text" class="form-control" id="PhoneNumber" name="PhoneNumber" placeholder="Phone Number" aria-label="Phone Number" aria-describedby="basic-addon1">
+            <div class="mb-3">
+                <div class="row d-flex align-items-center">
+                    <div class="col-md-3">
+                        <label for="image" class="form-label">
+                            <img src="/images/no-photo.png" alt="image" width="100%">
+                        </label>
+                    </div>
+                    <div class="mb-3 col-md-9">
+                        <input type="file" class="form-control" id="image" name="image" aria-describedby="emailHelp">
+                    </div>
+                </div>
             </div>
-            <div class="form-text">Enter your phone number</div>
-        </div>
-        <button class="btn btn-primary">Submit</button>
-</form>
-<script src="./js/bootstrap.bundle.min.js"/>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-success me-2">Створити</button>
+                <a href="/" class="btn btn-primary">Скасувать</a>
+            </div>
+        </form>
+    </div>
+</main>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
